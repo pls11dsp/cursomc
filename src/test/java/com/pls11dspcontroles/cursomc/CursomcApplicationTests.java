c5979536346e35c217daeb4755163fb569be479c
pls11dsp <pls11@dspcontroles.com.br>
2024-08-27 05:47:21 -0300

    Projeto Criado

---
+++ src/test/java/com/pls11dspcontroles/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pls11dspcontroles.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
