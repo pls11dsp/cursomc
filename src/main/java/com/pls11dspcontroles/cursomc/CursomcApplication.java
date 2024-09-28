553aa0be6eca2b51a28a627b1fa0191990c1d692
pls11dsp <pls11@dspcontroles.com.br>
2024-09-28 12:30:21 -0300

    Cliente, TipoCliente, telefones e endereços

--- src/main/java/com/pls11dspcontroles/cursomc/CursomcApplication.java
+++ src/main/java/com/pls11dspcontroles/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.pls11dspcontroles.cursomc.domain.Categoria;
 import com.pls11dspcontroles.cursomc.domain.Cidade;
+import com.pls11dspcontroles.cursomc.domain.Cliente;
+import com.pls11dspcontroles.cursomc.domain.Endereco;
 import com.pls11dspcontroles.cursomc.domain.Estado;
 import com.pls11dspcontroles.cursomc.domain.Produto;
+import com.pls11dspcontroles.cursomc.domain.enums.TipoCliente;
 import com.pls11dspcontroles.cursomc.repositories.CategoriaRepository;
 import com.pls11dspcontroles.cursomc.repositories.CidadeRepository;
+import com.pls11dspcontroles.cursomc.repositories.ClienteRepository;
+import com.pls11dspcontroles.cursomc.repositories.EnderecoRepository;
 import com.pls11dspcontroles.cursomc.repositories.EstadoRepository;
 import com.pls11dspcontroles.cursomc.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -66,5 +77,17 @@ public class CursomcApplication implements CommandLineRunner{
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+	
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 313", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+	    
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 }
---
+++ src/main/java/com/pls11dspcontroles/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,36 @@
+package com.pls11dspcontroles.cursomc.domain.enums;
+
+public enum TipoCliente {
+	
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnun(Integer cod) {
+		
+		if(cod == null) {
+			return null;
+		}
+		for(TipoCliente x : TipoCliente.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
