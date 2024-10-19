31b87ad45be520dfc4c37616e1a79b7edf3b2479
pls11dsp <pls11@dspcontroles.com.br>
2024-10-19 16:57:32 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/pls11dspcontroles/cursomc/domain/Pagamento.java
+++ src/main/java/com/pls11dspcontroles/cursomc/domain/Pagamento.java
@@ -3,6 +3,7 @@ package com.pls11dspcontroles.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.pls11dspcontroles.cursomc.domain.enums.EstadoPagamento;
 
 import jakarta.persistence.Entity;
@@ -22,6 +23,7 @@ public abstract class Pagamento implements Serializable{
 	private Integer id;
 	private Integer estado;
 	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
